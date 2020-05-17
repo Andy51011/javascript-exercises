60043ea2559da9b1137285ecffe4ae8e09bf8d3c
Andy Yuen <Andyyuen287@gmail.com>
2020-05-17 02:09:51 -0700

    Finished repeatString

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,4 +1,17 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    var repeat = ""
+    var i = 0
+    if (num === 0) {
+        return ''
+    }
+    if (num < 0) {
+        return "ERROR"
+    }
+    while (i < num) {
+        repeat += string
+        i++
+    }
+    return repeat
 
 }
 
