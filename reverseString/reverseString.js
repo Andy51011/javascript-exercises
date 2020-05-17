46ec69680530665294417dfe78a09cacd4b67292
Andy Yuen <Andyyuen287@gmail.com>
2020-05-17 02:13:14 -0700

    Finished reverseString

--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,4 +1,7 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    var split = string.split("");
+    var reversed = split.reverse().join("");
+    return reversed;
 
 }
 
